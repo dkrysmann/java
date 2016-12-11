f02afb6f50fb895b9d2d559681bde65fa439deda
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-11 23:11:54 +0200

    BigDecimal

---
+++ src/RectangleArea.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 20;
+        int result = a * b;
+        BigDecimal firsNum = new BigDecimal("5");
+        BigDecimal secondValue = new BigDecimal("12");
+        System.out.println("Result = " + firsNum.add(secondValue));
+        System.out.println("Result is " + result);
+    }
+}
