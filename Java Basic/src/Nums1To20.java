06cf361acf6741f89a4662c86cc7b8ea74a0988b
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-12 21:38:50 +0200

    up

---
+++ Java Basic/src/Nums1To20.java
@@ -0,0 +1,9 @@
+
+public class Nums1To20 {
+    public static void main(String[] args) {
+        int a = 20;
+        for(int n = 1;n <= a; n++){
+            System.out.println(n);
+        }
+    }
+}
