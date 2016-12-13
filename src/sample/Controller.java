4caa6064406e71748e6a78ec49b89a3624081032
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-13 20:39:10 +0200

    update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class Controller {
 
+    public TextField num1;
+    public TextField num2;
+    public TextField result;
+
+    public void calculate(ActionEvent actionEvent) {
+                try {
+
+                    double firstNum = Double.parseDouble(num1.getText());
+                    double secondNum = Double.parseDouble(num2.getText());
+                    double resultInfo = firstNum + secondNum;
+                    result.setText(String.valueOf(resultInfo));
+                }
+                catch (Exception error){
+                    result.setText("Error");
+                }
+    }
 }
