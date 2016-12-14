827ca925f4d53def059e399d007bff4e030d7bb2
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-14 14:10:33 +0200

    up

---
+++ JavaSyntax/src/ternaryOperator.java
@@ -0,0 +1,10 @@
+
+public class ternaryOperator {
+    public static void main(String[] args) {
+        int minVal = (4 < 5) ? 4 : 5;
+        int maxVal = (5 > 6) ? 6 : 5;
+        System.out.println(minVal);
+        System.out.println(maxVal);
+    }
+
+}
