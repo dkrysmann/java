62113bbfac384f2315099caaec54c90acd45c33f
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-15 21:14:11 +0200

    update

---
+++ CollectionsInJava/src/Arrays.java
@@ -0,0 +1,12 @@
+
+public class Arrays {
+    public static void main(String[] args) {
+       String[] data = {
+               "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
+       };
+        for (int i = 0; i < data.length; i++) {
+            System.out.printf("names[%d] = %s\n", i, data[i]);
+        }
+
+    }
+}
---
+++ CollectionsInJava/src/setArrayRange.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class setArrayRange {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String[] nums = new String[n];
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = scan.nextLine();
+            }
+
+    }
+}
