5eadfe9a341e25af61cdfb84cfbafe5ba5e24825
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-25 01:32:28 +0200

    up

---
+++ ComplexCalc/src/volleyball.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class volleyball {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String isLeap = scanner.nextLine();
+        int holidays = scanner.nextInt();
+        int weekends = scanner.nextInt();
+        int weekendsNotHome = 48 - weekends;
+        double total = ((2.0 / 3.0) * holidays) + weekends + weekendsNotHome * (3.0 / 4.0);
+        double leap = 0.15 * total;
+        //
+
+        if (isLeap.matches("leap")) {
+            total += leap;
+        }
+
+        System.out.println((int)(Math.floor(total)));
+
+    }
+}
