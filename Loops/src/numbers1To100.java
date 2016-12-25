efe68d0c91c12bb33e4adaa2016a4f16b24ac3cc
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-25 10:48:12 +0200

    up

---
+++ Loops/src/numbers1To100.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class numbers1To100 {
+    public static void main(String[] args) {
+        for (int a = 1; a<= 100;a++){
+            System.out.println(a);
+        }
+    }
+}
---
+++ Loops/src/numbersEndingIn7.java
@@ -0,0 +1,10 @@
+
+public class numbersEndingIn7 {
+    public static void main(String[] args) {
+        for (int a = 1;a<= 1000;a++){
+            if(a % 10==7){
+                System.out.println(a);
+            }
+        }
+    }
+}
