bc0b75afefdcf61cc2bed2b0865a9e8cb49932f6
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-27 20:09:27 +0200

    up

--- DrawingFiguresWithLoops/src/simpleTest.java
+++ DrawingFiguresWithLoops/src/simpleTest.java
@@ -2,6 +2,7 @@
 public class simpleTest {
     public static void main(String[] args) {
         repeatStr("Hi",5);
+        repeatAgain("Hello",5);
     }
     static String repeatStr(String str,int count){
         String repeat = "";
@@ -10,5 +11,13 @@ public class simpleTest {
         }
         System.out.println(repeat);
         return  repeat;
+    }
+    static String repeatAgain(String again,int count){
+        String repeat = "";
+        for (int i = 0; i < count; i++) {
+            repeat += again;
+        }
+        System.out.println(repeat);
+        return repeat;
     }
 }
