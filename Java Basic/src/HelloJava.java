839723f60e5e6cd225d5953191cce784ffe13e68
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-12-30 00:01:42 +0200

    refresh

--- Java Basic/src/HelloJava.java
+++ Java Basic/src/HelloJava.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+      Scanner input = new Scanner(System.in);
+      int num1 = Integer.parseInt(input.nextLine());
+      int num2 = Integer.parseInt(input.nextLine());
+      calculation(num1,num2);
+    }
+    static int calculation(int a,int b){
+        int result = a + b;
+        System.out.println("The result is = " + result);
+        return result;
     }
 }
