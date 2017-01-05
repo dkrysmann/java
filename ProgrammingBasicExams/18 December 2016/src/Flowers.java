347bd2012b28a23ea1d1b9f452f307d3492832e3
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-01-05 22:21:45 +0200

    up

--- ProgrammingBasicExams/18 December 2016/src/Flowers.java
+++ ProgrammingBasicExams/18 December 2016/src/Flowers.java
@@ -1,4 +1,4 @@
-import com.sun.deploy.resources.Deployment_pt_BR;
+
 
 import java.util.Scanner;
 
@@ -10,45 +10,41 @@ public class Flowers {
         double laleta = Double.parseDouble(input.nextLine());
         String season = input.nextLine();
        String special = input.nextLine();
-       double hrizantemaSpringSummer = 0;
-       double hrizantemaAutumnWinter = 0;
-       double roseSpringSummer = 0;
-       double rosesAutumnWinter = 0;
-       double laleSpringSummer = 0;
-       double laleAutumnWinter = 0;
        double prepared = 2;
-       switch (season){
-           case "Spring":
-               hrizantemaSpringSummer = 2;
-               roseSpringSummer = 4.10;
-               laleSpringSummer = 2.50;
-               double price = (hrizantemas*hrizantemaSpringSummer) + (roses * roseSpringSummer) + (laleta
-               * laleSpringSummer);
-               if(special.equals("Y")){
-                   double specialDay = ((price / 100) * 15);
-                   double newPrice = price + specialDay;
-                   if(laleta >= 7){
-                       double allowance = ((newPrice / 100) * 5);
-                       double secondPrice = (newPrice - allowance) + prepared;
-                       System.out.printf("%.2f",secondPrice);
-                   }
-               }else if((hrizantemas + roses + laleta) <= 20){
-                   System.out.printf("%.2f",price + prepared);
-               }
-               break;
-           case "Summer":
-               hrizantemaSpringSummer = 2;
-               roseSpringSummer = 4.10;
-               laleSpringSummer = 2.50;
-               double priceS = (hrizantemas*hrizantemaSpringSummer) + (roses * roseSpringSummer) + (laleta
-                       * laleSpringSummer);
-               if(special.equals("Y")){
-                   double specialDay = ((priceS / 100) * 15);
-                   double newPrice = priceS + specialDay;
-                   System.out.printf("%.2f",newPrice);
-               }else{
-                   System.out.printf("%.2f",priceS + prepared);
-               }
+       double priceInHolidays = 0.15;
+            if(season.equals("Spring") || season.equals("Summer")) {
+                double hrizantemasPrice = 2.00;
+                double rosesPrice = 4.10;
+                double laletaPrice = 2.50;
+                double allPrice = (hrizantemas * hrizantemasPrice) + (roses * rosesPrice) + (laleta * laletaPrice);
+                if (special.equals("Y")) {
+                    allPrice += allPrice * priceInHolidays;
+                }
+                if (laleta > 7) {
+                    allPrice -= allPrice * 0.05;
+                }
+                if ((hrizantemas + roses + laleta) > 20) {
+                    allPrice -= allPrice * 0.2;
+                }
+                double newPrice = allPrice + prepared;
+                System.out.printf("%.2f", newPrice);
+            }
+          else if(season.equals("Autumn") || season.equals("Winter")) {
+                double hrizantemasPriceW = 3.75;
+                double rosesPriceW = 4.50;
+                double laletaPriceW = 4.15;
+                double allPriceW = (hrizantemas * hrizantemasPriceW) + (roses * rosesPriceW) + (laleta * laletaPriceW);
+                if (special.equals("Y")) {
+                    allPriceW += allPriceW * priceInHolidays;
+                }
+                if (roses >= 10 && season.equals("Winter")) {
+                    allPriceW -= allPriceW * 0.1;
+                }
+                if ((hrizantemas + roses + laleta) > 20) {
+                    allPriceW -= allPriceW * 0.2;
+                }
+                double newPriceW = allPriceW + prepared;
+                System.out.printf("%.2f", newPriceW);
+            }
        }
     }
-}
