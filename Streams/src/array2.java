c96f7baf91470df5b8e6a2d840e412f06e1689dc
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-01-07 16:44:22 +0200

    ok

---
+++ Streams/src/array2.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class array2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = Integer.parseInt(input.nextLine());
+        int[]arr = new int[n];
+        printElements(arr);
+    }
+    static int[] printElements(int[]array1){
+        Scanner num = new Scanner(System.in);
+        for (int i = 0; i < array1.length; i++) {
+            int n = Integer.parseInt(num.nextLine());
+            array1[i]+=n;
+        }
+        Arrays.sort(array1);
+        System.out.println(Arrays.toString(array1));
+        return array1;
+    }
+}
