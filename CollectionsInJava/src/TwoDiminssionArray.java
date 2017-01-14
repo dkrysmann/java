f6d71170e1cfa65fb04b64519ffdd7bbe7d427c5
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-01-14 21:50:15 +0200

    up

--- CollectionsInJava/src/TwoDiminssionArray.java
+++ CollectionsInJava/src/TwoDiminssionArray.java
@@ -3,18 +3,15 @@ import java.util.Arrays;
 public class TwoDiminssionArray {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.printf("Enter rows:%n");
-        int rows = Integer.parseInt(input.nextLine());
-        System.out.printf("Enter cols:%n");
-        int cols = Integer.parseInt(input.nextLine());
-        System.out.printf("%d X %d%n",rows,cols);
-        int[][] twoDim = new int[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                twoDim[i][j] = input.nextInt();
+        int r = Integer.parseInt(input.nextLine());
+        int c = Integer.parseInt(input.nextLine());
+        int[][] first = new int[r][c];
+        for (int rows = 0; rows < r; rows++) {
+            for (int cols = 0; cols < c; cols++) {
+                first[rows][cols] = Integer.parseInt(input.nextLine());
             }
-            System.out.println();
         }
-        System.out.println(Arrays.deepToString(twoDim));
+        System.out.println(Arrays.deepToString(first));
+
     }
 }
