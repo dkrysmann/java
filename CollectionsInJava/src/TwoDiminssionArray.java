1c520709ab08a26a589c5c183a1cf86b84806950
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-01-14 22:34:57 +0200

    finally

--- CollectionsInJava/src/TwoDiminssionArray.java
+++ CollectionsInJava/src/TwoDiminssionArray.java
@@ -6,11 +6,18 @@ public class TwoDiminssionArray {
         int r = Integer.parseInt(input.nextLine());
         int c = Integer.parseInt(input.nextLine());
         int[][] first = new int[r][c];
+        System.out.printf("%dX%d%n",r,c);
         for (int rows = 0; rows < r; rows++) {
             for (int cols = 0; cols < c; cols++) {
                 first[rows][cols] = Integer.parseInt(input.nextLine());
             }
         }
-        System.out.println(Arrays.deepToString(first));
+       // System.out.println(Arrays.deepToString(first));
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(first[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
