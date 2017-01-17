36e6a8df0169e4e9187caacd98a298a4cad703fd
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-01-17 14:52:49 +0200

    update

--- Java Syntax = Excersise/src/TriangleArea.java
+++ Java Syntax = Excersise/src/TriangleArea.java
@@ -14,8 +14,8 @@ public class TriangleArea {
         calculateArea(Ax,Ay,Bx,By,Cx,Cy);
     }
     static double calculateArea(int Ax,int Ay,int Bx,int By,int Cx,int Cy){
-        double area = Math.abs(Ax * (By-Cy) + Bx * (Cy - Ay) + Cx * (Ay - By)) / 2;
-        System.out.printf("%.0f",area);
+        double area = (Ax * (By-Cy) + Bx * (Cy - Ay) + Cx * (Ay - By)) / 2;
+        System.out.printf("%.0f",Math.abs(area));
         return area;
     }
 }
