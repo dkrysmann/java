8610040581c16b6afba73d325afc8b1afa5a558b
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-01-26 23:40:55 +0200

    up

--- StepsAndMaps/src/ParkingLot.java
+++ StepsAndMaps/src/ParkingLot.java
@@ -1,4 +1,28 @@
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class ParkingLot {
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashSet<String> parking = new HashSet<>();
+        while (true){
+            String direction = input.nextLine();
+            if(direction.equals("END")){
+                break;
+            }
+            String[] command = direction.split(", ");
+            if(command[0].equals("IN")){
+                parking.add(command[1]);
+            }else{
+                parking.remove(command[1]);
+            }
+        }
+        if(parking.isEmpty()){
+            System.out.printf("Parking Lot is Empty");
+        }else{
+            for (String cars : parking) {
+                System.out.println(cars);
+            }
+        }
+    }
 }
