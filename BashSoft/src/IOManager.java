62d3cc15257fa28aa68ac544c88bad4938861f14
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-01-29 20:33:26 +0200

    up

--- BashSoft/src/IOManager.java
+++ BashSoft/src/IOManager.java
@@ -1,3 +1,33 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class IOManager {
+    public static void main(String[] args) {
+        traverseDirectory("D:\\");
+    }
+
+    public static void traverseDirectory(String path) {
+        LinkedList<File> subFolders = new LinkedList<File>();
+        File root = new File(path);
+
+        subFolders.add(root);
+
+        while (subFolders.size() != 0) {
+            File currentFolder = subFolders.removeFirst();
+
+            try {
+                if (currentFolder.listFiles() != null) {
+                    for (File file : currentFolder.listFiles()) {
+                        if (file.isDirectory() && file.listFiles() != null) {
+                            subFolders.addAll(Arrays.asList(file.listFiles()));
+                        }
+                    }
+                }
+                System.out.println(currentFolder.toString());
+            } catch (Exception e) {
+                System.out.print("Access denied");
+            }
+        }
+    }
 }
