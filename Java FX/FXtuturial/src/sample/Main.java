4ee6191ff951fef7bcae193dc130e38ad160ba78
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-01-30 19:56:54 +0200

    up

--- Java FX/FXtuturial/src/sample/Main.java
+++ Java FX/FXtuturial/src/sample/Main.java
@@ -1,38 +1,23 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("MyWindow");
-            primaryStage.setWidth(800);
-            primaryStage.setHeight(700);
-            Pane pane = new Pane();
-        Button btn = new Button();
-        btn.setText("Click");
-            btn.setTranslateX(140);
-            btn.setTranslateY(140);
-            btn.setPrefSize(70,20);
-        Rectangle rectangle = new Rectangle(100,100);
-            btn.setOnAction(event -> {
-               rectangle.setFill(Color.AZURE);
-            });
-        Scene scene = new Scene(pane);
-        pane.getChildren().addAll(btn,rectangle);
-            primaryStage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         launch(args);
     }
 }
---
+++ Java FX/Labels/src/sample/Main.java
@@ -0,0 +1,64 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Pane pane = new Pane();
+      //  Image spaceShip = new Image(getClass().getResourceAsStream("spaceship.png"));
+        File spaceShipSprite = new File("D:\\projects\\Java\\Java FX\\Labels\\src\\sprites\\spaceship.png");
+        File earthSprite = new File("D:\\projects\\Java\\Java FX\\Labels\\src\\sprites\\planet-earth.png");
+        File planetUranusSprite = new File("D:\\projects\\Java\\Java FX\\Labels\\src\\sprites\\planet-uranus.png");
+        File spacePlanetSprite = new File("D:\\projects\\Java\\Java FX\\Labels\\src\\sprites\\Space-Planet.png");
+
+        Image spaceShip = new Image(spaceShipSprite.toURI().toString());
+        ImageView imgSpaceShip = new ImageView(spaceShip);
+        imgSpaceShip.setFitHeight(90);
+        imgSpaceShip.setFitWidth(90);
+        imgSpaceShip.setX(400);
+        imgSpaceShip.setY(600);
+
+        Image planetEarth = new Image(earthSprite.toURI().toString());
+        ImageView imgPlanetEarth = new ImageView(planetEarth);
+        imgPlanetEarth.setFitHeight(150);
+        imgPlanetEarth.setFitWidth(150);
+        imgPlanetEarth.setX(100);
+        imgPlanetEarth.setY(300);
+
+        Image planetUranus = new Image(planetUranusSprite.toURI().toString());
+        ImageView imgUranus = new ImageView(planetUranus);
+        imgUranus.setFitHeight(150);
+        imgUranus.setFitWidth(150);
+        imgUranus.setX(340);
+        imgUranus.setY(100);
+
+        Image planetSaturn = new Image(spacePlanetSprite.toURI().toString());
+        ImageView imgSaturn = new ImageView(planetSaturn);
+        imgSaturn.setFitHeight(100);
+        imgSaturn.setFitWidth(100);
+        imgSaturn.setX(800);
+        imgSaturn.setY(100);
+
+        pane.getChildren().addAll(imgSpaceShip,imgPlanetEarth,imgUranus,imgSaturn);
+        Scene scene = new Scene(pane,1024,1024);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
