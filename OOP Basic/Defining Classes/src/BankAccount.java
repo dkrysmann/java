b6406a04958b6686ff1089e7590eae4d8a66085d
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-02-22 15:13:17 +0200

    BankAccountUp

---
+++ OOP Basic/Defining Classes/src/BankAccount.java
@@ -0,0 +1,5 @@
+
+public class BankAccount {
+    int id;
+    double balance;
+}
--- OOP Basic/Defining Classes/src/Demo.java
+++ OOP Basic/Defining Classes/src/Demo.java
@@ -1,6 +1,11 @@
 
 public class Demo {
     public static void main(String[] args) {
-
+        BankAccount account = new BankAccount();
+        account.id = 1;
+        account.balance = 15;
+        System.out.printf("Account %d, balance: %.2f",
+                account.id,
+                account.balance);
     }
 }
