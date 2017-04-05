42c2f9761829a1ac87c660ea1510bd645e920e22
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-04-05 22:38:36 +0300

    Junit demo

---
+++ OOP Advance/UnitTesting/src/test/BankAccountTest.java
@@ -0,0 +1,16 @@
+package test;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BankAccountTest {
+    @Test
+    public void testDepositBankAccount(){
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void testWithDrawBankAccount(){
+        Assert.assertTrue(true);
+    }
+}
