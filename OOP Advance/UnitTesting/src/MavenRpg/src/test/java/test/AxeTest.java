96a9da7be66c40bc268c24bb56a29028928784a8
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-04-07 14:13:34 +0300

    Refactoring complete

--- OOP Advance/UnitTesting/src/MavenRpg/src/test/java/test/AxeTest.java
+++ OOP Advance/UnitTesting/src/MavenRpg/src/test/java/test/AxeTest.java
@@ -1,25 +1,39 @@
 package test;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import rpg_lab.Axe;
 import rpg_lab.Dummy;
 
 public class AxeTest {
+
+    private static final int AXE_ATTACK = 10;
+    private static final int AXE_DURABILITY = 10;
+    private static final int DUMMY_HEALTH = 10;
+    private static final int DUMMY_EXPIRINCE = 10;
+    private static final int EXPECTED_DURABILITY = 9;
+
+    private Axe testAxe;
+    private Dummy testDummy;
+
+    @Before
+    public void initializeTestObjects(){
+        this.testAxe = new Axe(AXE_ATTACK,AXE_DURABILITY);
+        this.testDummy = new Dummy(DUMMY_HEALTH,DUMMY_EXPIRINCE);
+    }
+
     @Test
     public void testAxeDurability(){
         //arange
-        Axe testAxe = new Axe(10,10);
-        Dummy testDummy = new Dummy(10,10);
-        //act
-        testAxe.attack(testDummy);
+       testAxe.attack(testDummy);
         //assert
-        Assert.assertTrue(testAxe.getDurabilityPoints() == 9);
+        Assert.assertEquals("Wrong durability",EXPECTED_DURABILITY,this.testAxe.getDurabilityPoints());
     }
     @Test(expected = IllegalStateException.class)
     public void testBrokenAxeAttack(){
-        Axe testAxe = new Axe(10,0);
-        Dummy testDummy = new Dummy(10,10);
-        testAxe.attack(testDummy);
+       while (this.testAxe.getDurabilityPoints() > 0){
+           this.testAxe.attack(testDummy);
+       }
     }
 }
